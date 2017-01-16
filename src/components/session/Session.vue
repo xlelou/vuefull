72e6e1e4bee0823349ab249ccd457ae92567a1c8
lybenson <naivebenson@gmail.com>
2017-01-16 23:51:03 +0800

    session

--- src/components/session/Session.vue
+++ src/components/session/Session.vue
@@ -4,11 +4,11 @@
 			<input name="" class="msg-input" v-model="content">
 			<span class="msg-send" @click="send">发送</span>
 		</div>
-		<div v-for="content in allContent">
-			<Feedback :content="content.content"></Feedback>
+		<div class="session-wrapper" v-for="content in allContent">
+			<Feedback :content="content.content" v-if="content.type==0"></Feedback>
 			<div style="height: 10px"></div>
+			<RevFeedback :content="content.content" v-if="content.type==1"></RevFeedback>
 		</div>
-		<RevFeedback :content="content.content"></RevFeedback>
 	</div>
 </template>
 
@@ -19,7 +19,7 @@ export default {
 	data() {
 		return {
 			content: "",
-			allContent: [{content: '2016年6月21日 - vuejsLearn--- v-for列表渲染 Vue.js是一个构建数据驱动的web界面的库。Vue.js 的核心是一个响应的数据绑定系统,它让数据与DOM保持同步非常简单如下列表展示'}]
+			allContent: [{type: 0, content: '2016年6月21日 - vuejsLearn--- v-for列表渲染 Vue.js是一个构建数据驱动的web界面的库。Vue.js 的核心是一个响应的数据绑定系统,它让数据与DOM保持同步非常简单如下列表展示'}]
 		}
 	},
 	components: {
@@ -30,6 +30,7 @@ export default {
 		send() {
 			if (this.content !== '') {
 				this.allContent.push({
+					type: 1,
 					content: this.content
 				})
 				this.content = ''
@@ -44,10 +45,13 @@ export default {
 		margin-bottom 49px
 		vertical-align middle
 		.input-wrapper
+			position fixed
+			top 0px
 			width 100%
 			height 30px
 			line-height 30px
 			margin-bottom 10px
+			background white
 			.msg-input
 				border 1px solid black
 				width 70%
@@ -58,4 +62,6 @@ export default {
 				height 100%
 				text-align center
 				background green
+		.session-wrapper
+			margin-top 40px
 </style>
