b52ec83113ccf10076a05fbbe8b83a34b0846397
lybenson <naivebenson@gmail.com>
2017-02-23 23:50:51 +0800

    o

--- src/api/robotApi.js
+++ src/api/robotApi.js
@@ -3,12 +3,10 @@ import { sessionUrl } from './urlConfig.js'
 
 export default {
  	getFeedback(data) {
- 		console.log(data + 'data-----')
  		return axios.get('/robotfeedback', {
  				params: {
  					info: data 				}
  			}).then(function(response) {
- 				console.log(response.data)
  				return response.data
  			})
  		}
